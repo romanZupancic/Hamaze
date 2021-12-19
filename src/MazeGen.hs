9ec052b0b7f77d6cce5ba69f4f5d00abaab36086
Roman Zupancic <roman.k.zupancic@gmail.com>
2021-12-18 21:38:07 -0500

    Reformatted documentation in mazgen module.

--- src/MazeGen.hs
+++ src/MazeGen.hs
@@ -19,8 +19,8 @@ data GridCell a = GridCell { cellX :: Int -- ^ The x-position of the cell
     deriving Show
 
 -- | A grid, a collection of cells
-data Grid a = Grid { gridWidth    :: Int -- ^ The width of the grid (in cells)
-                   , gridHeight   :: Int -- ^ The height of the grid (in cells)
+data Grid a = Grid { gridWidth    :: Int            -- ^ The width of the grid (in cells)
+                   , gridHeight   :: Int            -- ^ The height of the grid (in cells)
                    , gridContents :: [[GridCell a]] -- ^ The cells in the grid
                    }
     deriving Show
@@ -30,20 +30,20 @@ type MazeGrid = Grid MazeCell
 
 -- | Generate a blank maze-specific grid
 generateMazeGrid :: (Int, Int) -- ^ Width x Height
-                 -> MazeGrid -- ^ Width x Height dimensioned maze grid
+                 -> MazeGrid   -- ^ Width x Height dimensioned maze grid
 generateMazeGrid (x, y) = Grid x y [[defGridCell xv yv | xv <- [0..(x - 1)]] | yv <- [0..(y - 1)]]
     where
         defGridCell cellx celly = GridCell cellx celly (MazeCell True True True True False)
 
 -- | Extract a gridcell from a grid
-gridSelect :: Grid a -- ^ The grid
+gridSelect :: Grid a     -- ^ The grid
            -> (Int, Int) -- ^ The x and y coordinates of the desired item
            -> GridCell a -- ^ The desired gridcell
 gridSelect grid (x, y) = (gridContents grid) !! y !! x
 
 -- | Deduce the coordinates of the top, bottom, left, and right cells, if they exist
-surroundingCoords :: (Int, Int) -- ^ The x-coordinate of the inspecting cell
-                  -> (Int, Int) -- ^ The (width, height) of the grid the cell exists in
+surroundingCoords :: (Int, Int)   -- ^ The x-coordinate of the inspecting cell
+                  -> (Int, Int)   -- ^ The (width, height) of the grid the cell exists in
                   -> [(Int, Int)] -- ^ A list of vertical and horizontal immediate neighbours
 surroundingCoords (x, y) bounds = [(vx, y) | vx <- approvedX] ++ [(x, vy) | vy <- approvedY]
     where
@@ -51,10 +51,10 @@ surroundingCoords (x, y) bounds = [(vx, y) | vx <- approvedX] ++ [(x, vy) | vy <
         approvedY = (if y /= 0 then [y - 1] else []) ++ (if y /= (snd bounds) - 1 then [y + 1] else [])
 
 -- | Remove the wall between two (neighbouring) cells
-demolishWall :: MazeGrid -- ^ The mazegrid to "modify"
+demolishWall :: MazeGrid   -- ^ The mazegrid to "modify"
              -> (Int, Int) -- ^ The coordinates of the first cell
              -> (Int, Int) -- ^ The coordinates of the second cell
-             -> MazeGrid -- ^ The resulting maze grid
+             -> MazeGrid   -- ^ The resulting maze grid
 demolishWall mazeGrid one@(x, y) two@(nx, ny) = sndReplacement
     where
         makeNewCell mzeCell dx dy
@@ -69,10 +69,10 @@ demolishWall mazeGrid one@(x, y) two@(nx, ny) = sndReplacement
             (makeNewCell (info $ gridSelect fstReplacement two) (nx - x) (ny - y))
 
 -- | Generate a maze
-generateMaze :: MazeGrid -- ^ An inital maze to "modify"
+generateMaze :: MazeGrid   -- ^ An inital maze to "modify"
              -> (Int, Int) -- ^ The coordinates of the cell whose walls will be removed/traversed across
-             -> [Int] -- ^ A stream of numbers to determine traversal path
-             -> MazeGrid -- ^ A mazed grid
+             -> [Int]      -- ^ A stream of numbers to determine traversal path
+             -> MazeGrid   -- ^ A mazed grid
 generateMaze mazeGrid (x, y) neighbourOrder =
   foldr (connectCells) markedVisited neighboursReordered
     where
@@ -88,10 +88,10 @@ generateMaze mazeGrid (x, y) neighbourOrder =
                                        generateMaze (demolishWall mze (x, y) (ix, iy)) (ix, iy) nxt
 
 -- | Replace the information of a specific cell in a grid
-replaceMaze :: Grid a -- | The grid whose cell contents will be replaced
-            -> (Int, Int) -- | The coordinates of the target cell
-            -> a -- | The data to replace with
-            -> Grid a -- | The new grid
+replaceMaze :: Grid a     -- ^ The grid whose cell contents will be replaced
+            -> (Int, Int) -- ^ The coordinates of the target cell
+            -> a          -- ^ The data to replace with
+            -> Grid a     -- ^ The new grid
 replaceMaze mze@Grid { gridContents = ctns } (x, y) ele = mze { gridContents = newGrid}
     where
         newCell = (gridSelect mze (x, y)) { info = ele }
@@ -110,9 +110,9 @@ genRandomList gen range@(start, end) = value : genRandomList nextGen range
 
 -- | Reorder items according to some weird algorithm that just kinda
 -- works. Don't rely on it for anything but wonky results.
-reorder :: [a] -- ^ The source of the items
+reorder :: [a]   -- ^ The source of the items
         -> [Int] -- ^ Numbers that control how the list is reordered
-        -> [a] -- ^ The reordered list
+        -> [a]   -- ^ The reordered list
 reorder items [] = items
 reorder items (x:xs) = (items !! extractIndex) : reorder remainingItems xs
     where
